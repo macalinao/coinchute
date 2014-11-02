81bd1f90e29afee126012a10fd40cd1e2eef0541
Ian Macalinao <ian@giza.us>
2014-11-02 01:23:20 -0500

    Finish up dashboard

--- client/app.js
+++ client/app.js
@@ -25,27 +25,35 @@ angular.module('coinchute', ['ui.router'])
 .controller('DashboardCtrl', function($scope) {
   $scope.account = {
     balance: 0.89,
-    balanceDollars: 340
+    balanceDollars: 340,
+    address: '1HB5XMLmzFVj8ALj6mfBsbifRoD4miY36v'
   };
 
-  $scope.scheduled = [
-    {
-      company: 'Quizlet',
-      companyImage: 'http://quizlet.com/a/i/icons/512.EBT7.jpg',
-      amount: 9.99,
-      period: 'month',
-      item: 'Quizlet Premium',
-      last: '2014-10-02',
-      next: '2014-11-02'
-    },
-    {
-      company: 'Spotify, Inc.',
-      companyImage: 'http://www.mobileworldlive.com/wp-content/uploads/2013/05/spotify-logo.jpg',
-      amount: 4.99,
-      period: 'month',
-      item: 'Spotify Premium',
-      last: '2014-10-02',
-      next: '2014-11-02'
-    }
-  ];
+  $scope.scheduled = [{
+    company: 'Quizlet',
+    companyImage: 'http://quizlet.com/a/i/icons/512.EBT7.jpg',
+    amount: 9.99,
+    period: 'month',
+    item: 'Quizlet Premium',
+    last: '2014-10-02',
+    next: '2014-11-02'
+  }, {
+    company: 'Spotify, Inc.',
+    companyImage: 'http://www.mobileworldlive.com/wp-content/uploads/2013/05/spotify-logo.jpg',
+    amount: 4.99,
+    period: 'month',
+    item: 'Spotify Premium',
+    last: '2014-10-02',
+    next: '2014-11-02'
+  }];
+
+  var qrcode = new QRCode("qrcode", {
+    text: $scope.account.address,
+    width: 175,
+    height: 175,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H
+  });
+
 });
