4b1dc3253ddf585e17b412da10d5d89a7ac76ced
Andrew Tian <andrew@tian.io>
2014-11-01 20:46:20 -0700

    bad subscriptions

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Pull payment platform for Bitcoin.
 
 Bitcoin only supports push transactions, as in the buyer needs to initiate the payment via a program. This model does not work for recurring payments such as Spotify. PullCoin allows merchants to pull money from buyers via preauthorized transactions.
 
+API
+===
+
+- messy input validation at the moment
+- no authentication
--- api/main.go
+++ api/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"code.google.com/p/go-uuid/uuid"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/conformal/btcec"
 	"github.com/conformal/btcnet"
@@ -10,10 +12,16 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"time"
+)
+
+const (
+	hostname = "http://localhost:8080/"
 )
 
 var (
-	curNet = &btcnet.MainNetParams
+	curNet        = &btcnet.MainNetParams
+	subscriptions = []*Subscription{}
 )
 
 func main() {
@@ -24,10 +32,88 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/subscriptions", newSubscriptionHandler).Methods("POST")
+	r.HandleFunc("/subscriptions/{subUUID}", getSubscriptionHandler).Methods("GET")
+
 	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
+func writeJson(w http.ResponseWriter, m interface{}) {
+	if err := json.NewEncoder(w).Encode(m); err != nil {
+		log.Fatalln("could not write response")
+	}
+}
+
+type Subscription struct {
+	UUID      uuid.UUID `json:"uuid"`
+	CreatedAt time.Time `json:"created_at"`
+
+	Active       bool
+	MerchantID   string `json:"merchant_id"`
+	MerchantName string `json:"merchant_name"`
+	Name         string `json:"name"`
+	Quantity     int64  `json:"quantity"`
+	Interval     string `json:"interval"`
+}
+
+type NewSubRequest struct {
+	Subscription
+}
+
+type SubscriptionResponse struct {
+	*Subscription
+	Link string `json:"link"`
+}
+
+func NewSubResponse(sub *Subscription) *SubscriptionResponse {
+	fmt.Println(sub.UUID.String())
+	link := hostname + "subscriptions/" + sub.UUID.String()
+	return &SubscriptionResponse{sub, link}
+}
+
+func NewSubscription(merchId string, merchName string, name string, qty int64, interval string) *Subscription {
+	return &Subscription{
+		UUID:         uuid.NewUUID(),
+		CreatedAt:    time.Now(),
+		MerchantID:   merchId,
+		MerchantName: merchName,
+		Name:         name,
+		Quantity:     qty,
+		Interval:     interval,
+	}
+}
+
+func newSubscriptionHandler(w http.ResponseWriter, r *http.Request) {
+	var subr NewSubRequest
+	if err := json.NewDecoder(r.Body).Decode(&subr); err != nil {
+		log.Fatalln("unable to decode json")
+		return
+	}
+
+	sub := NewSubscription(subr.MerchantID, subr.MerchantName, subr.Name, subr.Quantity, subr.Interval)
+
+	if sub.MerchantID == "" || sub.MerchantName == "" ||
+		sub.Name == "" || sub.Quantity == 0 {
+		fmt.Fprintln(w, "validation error this is not json")
+		return
+	}
+
+	subscriptions = append(subscriptions, sub)
+
+	writeJson(w, NewSubResponse(sub))
+}
+
+func getSubscriptionHandler(w http.ResponseWriter, r *http.Request) {
+	uuid := mux.Vars(r)["subUUID"]
+
+	for _, sub := range subscriptions {
+		if sub.UUID.String() == uuid {
+			writeJson(w, NewSubResponse(sub))
+			return
+		}
+	}
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
