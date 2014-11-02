72e3957ff20968b55f745e6c079d5240cb6fe21f
Ian Macalinao <ian@giza.us>
2014-11-02 00:19:42 -0500

    Add scheduled payments screen

---
+++ client/app.js
@@ -0,0 +1,46 @@
+angular.module('coinchute', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  .state('home', {
+    url: '/',
+    templateUrl: 'templates/home.html',
+    controller: 'HomeCtrl'
+  })
+
+  .state('dashboard', {
+    url: '/dashboard',
+    templateUrl: 'templates/dashboard.html',
+    controller: 'DashboardCtrl'
+  });
+
+  $urlRouterProvider.otherwise('/');
+
+})
+
+.controller('HomeCtrl', function($scope) {})
+
+.controller('DashboardCtrl', function($scope) {
+  $scope.scheduled = [
+    {
+      company: 'Quizlet',
+      companyImage: 'http://quizlet.com/a/i/icons/512.EBT7.jpg',
+      amount: 9.99,
+      period: 'month',
+      item: 'Quizlet Premium',
+      last: '2014-10-02',
+      next: '2014-11-02'
+    },
+    {
+      company: 'Spotify, Inc.',
+      companyImage: 'http://www.mobileworldlive.com/wp-content/uploads/2013/05/spotify-logo.jpg',
+      amount: 4.99,
+      period: 'month',
+      item: 'Spotify Premium',
+      last: '2014-10-02',
+      next: '2014-11-02'
+    }
+  ];
+});
